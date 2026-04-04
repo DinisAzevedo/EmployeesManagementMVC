40e28f0d2301a158fb75d7b8885dc0682f229c6c
DinisAzevedo <a14530@oficina.pt>
2026-04-04 13:28:28 +0100

    Add project files.

---
+++ 14530_employes_managment/Models/UserActivity.cs
@@ -0,0 +1,12 @@
+﻿namespace _14530_employes_managment.Models
+{
+    public class UserActivity
+    {
+        public string? CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public string? ModifiedById { get; set; }
+        public DateTime ModifiedOn { get; set; }
+
+    }
+}
