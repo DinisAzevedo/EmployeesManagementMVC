8f999e153007a353e98d1f372d0aa80dca63a506
Dinis Azevedo <a14530@oficina.pt>
2026-05-15 10:22:33 +0000

    Changes

---
+++ 14530_employes_managment/Data/Migrations/20260515100337_changes.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _14530_employes_managment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Employees_Department",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "Department",
+                table: "Employees",
+                newName: "Function");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "UseStrings",
+                table: "Instruments",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Employees_Function",
+                table: "Employees",
+                sql: "[Function] IN ('Administration','Engineer','Worker')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Employees_Function",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "Function",
+                table: "Employees",
+                newName: "Department");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "UseStrings",
+                table: "Instruments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Employees_Department",
+                table: "Employees",
+                sql: "[Department] IN ('Administration','Engineer','Worker')");
+        }
+    }
+}
